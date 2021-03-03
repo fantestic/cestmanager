72e9cddbbfdbdd60122b91ee867f464a68309a80
Gerald Baumeister <gerald.b@whosonlocation.com>
2021-03-03 19:32:03 +1300

    Create Contracts for Collection/Scenario/Step

---
+++ src/Contract/CollectionInterface.php
@@ -0,0 +1,20 @@
+<?php
+
+declare(strict_types = 1);
+namespace Fantestic\CestManager\Contract;
+
+
+/**
+ * 
+ * 
+ * @package Fantestic/CestManager
+ * @author Gerald Baumeister <gerald@fantestic.io>
+ * @license http://www.apache.org/licenses/LICENSE-2.0
+ */
+interface CollectionInterface
+{
+    /**
+     * @return ScenarioInterface[]
+     */
+    public function getScenarios() :iterable;
+}
